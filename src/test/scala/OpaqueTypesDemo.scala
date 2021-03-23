6e4fad019e9f97914d6d3028e9c0bdba2a2e7938
Magda Stozek <magda.stozek@gmail.com>
2021-03-23 07:14:41 +0100

    Step 2 - raw types

--- src/test/scala/OpaqueTypesDemo.scala
+++ src/test/scala/OpaqueTypesDemo.scala
@@ -4,9 +4,12 @@ import java.time.LocalDate
 
 class OpaqueTypesDemo extends AnyWordSpec with Matchers {
 
+  case class Book(title: String, pages: Int, published: Int)
+
   "Opaque Types" should {
     "work" in {
-
+      val book = Book("Pippi Longstocking", 2010, 350)
+      book.pages should be(350)
     }
   }
 }

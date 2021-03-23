08487a55eada101c1ee1ca861a3b33ebcc724d4b
Magda Stozek <magda.stozek@gmail.com>
2021-03-23 07:15:56 +0100

    Step 4 - move out to object

--- src/test/scala/OpaqueTypesDemo.scala
+++ src/test/scala/OpaqueTypesDemo.scala
@@ -1,14 +1,17 @@
+import books._
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import java.time.LocalDate
 
-class OpaqueTypesDemo extends AnyWordSpec with Matchers {
+object books {
 
   case class Book(title: String, pages: Pages, published: Year)
 
   opaque type Pages = Int
   opaque type Year = Int
+}
 
+class OpaqueTypesDemo extends AnyWordSpec with Matchers {
   "Opaque Types" should {
     "work" in {
       val book = Book("Pippi Longstocking", 350, 2010)

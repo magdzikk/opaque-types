d3db4aea92357596d4a6323bc7da4ff4d984283e
Magda Stozek <magda.stozek@gmail.com>
2021-03-23 08:21:42 +0100

    Step 5 - construction methods

--- src/test/scala/OpaqueTypesDemo.scala
+++ src/test/scala/OpaqueTypesDemo.scala
@@ -8,13 +8,23 @@ object books {
   case class Book(title: String, pages: Pages, published: Year)
 
   opaque type Pages = Int
+
+  object Pages {
+    def apply(value: Int): Pages = value
+  }
+
   opaque type Year = Int
+
+  object Year {
+    def apply(value: Int): Year = value
+  }
+
 }
 
 class OpaqueTypesDemo extends AnyWordSpec with Matchers {
   "Opaque Types" should {
     "work" in {
-      val book = Book("Pippi Longstocking", 350, 2010)
+      val book = Book("Pippi Longstocking", Pages(350), Year(2010))
       book.pages should be(350)
     }
   }

ee6e6eab35288d5b0c9e7f1c856f740f55ce639a
Magda Stozek <magda.stozek@gmail.com>
2021-03-23 08:04:11 +0100

    Step 6 - safe construction

--- src/test/scala/OpaqueTypesDemo.scala
+++ src/test/scala/OpaqueTypesDemo.scala
@@ -11,12 +11,16 @@ object books {
 
   object Pages {
     def apply(value: Int): Pages = value
+
+    def safe(value: Int): Option[Pages] = if (value > 0) Some(value) else None
   }
 
   opaque type Year = Int
 
   object Year {
     def apply(value: Int): Year = value
+
+    def safe(value: Int): Option[Year] = if (value > 1450 && value < 2021) Some(value) else None
   }
 
 }
@@ -26,6 +30,18 @@ class OpaqueTypesDemo extends AnyWordSpec with Matchers {
     "work" in {
       val book = Book("Pippi Longstocking", Pages(350), Year(2010))
       book.pages should be(350)
+
+      val invalid = for {
+        pages <- Pages.safe(2010)
+        year <- Year.safe(350)
+      } yield Book("Calvin and Hobbes", pages, year)
+      invalid shouldBe None
+
+      val valid = for {
+        pages <- Pages.safe(350)
+        year <- Year.safe(2010)
+      } yield Book("Calvin and Hobbes", pages, year)
+      valid shouldBe defined
     }
   }
 }
